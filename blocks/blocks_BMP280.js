2cafcf01c28db183abe9d05bb568ed0470bfdf9c
Max <max30012540@hotmail.com>
2020-04-18 15:44:27 +0700

    Update blocks_BMP280.js

--- blocks/blocks_BMP280.js
+++ blocks/blocks_BMP280.js
@@ -2,7 +2,7 @@ Blockly.Blocks['bmp280_read_temperature'] = {
 	init: function() {
 		this.jsonInit({
 			"type": "bmp280_read_temperature",
-			"message0": "BMP280 address %1 read temperature (*C)",
+			"message0": "BMP280 at %1 read temperature (*C)",
 			"args0": [{
 				"type": "field_dropdown",
 				"name": "addr",
@@ -23,7 +23,7 @@ Blockly.Blocks['bmp280_read_pressure'] = {
 	init: function() {
 		this.jsonInit({
 			"type": "bmp280_read_pressure",
-			"message0": "BMP280 address %1 read pressure (Pa)",
+			"message0": "BMP280 at %1 read pressure (Pa)",
 			"args0": [{
 				"type": "field_dropdown",
 				"name": "addr",
